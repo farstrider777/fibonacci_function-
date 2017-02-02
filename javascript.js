cc169710c2ffb14ddfd4147fd2cd8b84e8333901
Joshua Bradford <jjyet@yahoo.com>
2017-02-01 23:13:17 -0500

    Added a second version of recursive fibonacci function that runs faster and stores the sequence in an array

--- javascript.js
+++ javascript.js
@@ -7,3 +7,21 @@ function computeFibonacciNumber(fibPosition){
 		return computeFibonacciNumber(fibPosition - 1) + computeFibonacciNumber(fibPosition - 2)
 	}
 }
+
+
+var fun = [1]
+
+function fact (n) {
+	var loop = function (product, count, max) {
+		if (count > max) {
+			return product;
+		} else {
+			fun.push(product);
+			return loop(product + fun[count], count + 1 , max);
+		}
+	};
+return loop(0, 0, n - 1);
+}
+
+console.log(fact(7))
+console.log(fun)
--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ Skills practiced
 1. Javascript
   * Wrote and tested a recursive function.
   * Followed and modified data flow through a recursive function.
+  * Rewrote my recursive function to be a linear recursive function which saves my Fibonacci sequence to an array at each step, thus reducing work on the computer and allowing it to run larger numbers quickly.
